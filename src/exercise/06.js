da4a430b91b2401f065131fd9ad1470138d8adc9
Ivy Olamit <ivy.o@getjobber.com>
2021-02-13 17:04:05 -0800

    Extra Credit 06: use React.useState to handle the lowercase validation.

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,21 +5,31 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 📜 https://reactjs.org/docs/hooks-reference.html#useref
-  const usernameInputRef = React.useRef();
+  // 📜 https://reactjs.org/docs/hooks-state.html
+  // const usernameInputRef = React.useRef();
+  const [error, setError] = React.useState(null);
 
-  function handleSubmit(event) {
+  function handleSubmit(event) {    
     event.preventDefault();
-    const value = usernameInputRef.current.value;
+    // const value = usernameInputRef.current.value;
+    const value = event.target.elements.usernameInput?.value;
     onSubmitUsername(value);
+  }
+
+  function handleChange(event) {
+    const {value} = event.target;
+    const isValid = value === value?.toLowerCase();
+    setError(isValid ? null : "User must be lower case");
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input ref={usernameInputRef} id="usernameInput" type="text" />
+        <input id="usernameInput" type="text" onChange={handleChange} />
       </div>
-      <button type="submit">Submit</button>
+      <div style={{color: 'red'}}>{error}</div>
+      <button disabled={Boolean(error)} type="submit">Submit</button>
     </form>
   )
 }
