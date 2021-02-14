567ee24616fa4a09e9c1964e2be3cca646db9107
Ivy Olamit <ivy.o@getjobber.com>
2021-02-13 16:30:03 -0800

    Extra Credit 05: add sizeless box

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -17,6 +17,7 @@ function Box({className = "", style, size, color = "", ...props}) {
 const smallBox = <Box size="small" color="lightblue">small lightblue box</Box>;
 const mediumBox = <Box size="medium" color="pink">medium pink box</Box>;
 const largeBox = <Box size="large" color="orange">large orange box</Box>;
+const sizelessBox = <Box>sizeless box</Box>;
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      {sizelessBox}
     </div>
   )
 }
